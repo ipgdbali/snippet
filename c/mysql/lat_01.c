fee74c5e9159f62a846b47bb2102cfc1b89bcd31
I Putu Gede Darmawan S.Kom <ipgdbali@users.noreply.github.com>
2019-04-13 15:47:23 +0800

    Create lat_01.c

---
+++ c/mysql/lat_01.c
@@ -0,0 +1,56 @@
+//MYSQL Snippet Code
+//Latihan 01
+//Listing Database for supplied host, username and password
+
+#include <stdio.h>
+#include <mysql.h>
+
+int main(int argc,char * argv[])
+{
+	MYSQL inst;
+	MYSQL_RES *result;
+	MYSQL_ROW row;
+
+	if(argc != 4)
+	{
+		printf("usage : %s <host> <username> <password>\n",argv[0]);
+		return -1;
+	}
+
+	mysql_init(&inst);
+	// Connecting to MySQL Server
+	if(!mysql_real_connect(&inst,argv[1],argv[2],argv[3],NULL,0,NULL,0))
+	{
+		fprintf(stderr,"Error Connecting %s",mysql_error(&inst));
+		return -1;
+	}
+
+	if(0 != mysql_query(&inst,"SHOW DATABASES"))
+	{
+		fprintf(stderr,"Error Querying  %s",mysql_error(&inst));
+		return -1;
+	};
+
+	//Use Result
+	result = mysql_use_result(&inst);
+	if(!result)
+	{
+		fprintf(stderr,"No Data is Gathered%s",mysql_error(&inst));
+		return -1;
+	}
+
+
+	printf("List Database : \n");
+	while(row = mysql_fetch_row(result))
+	{
+		printf("%s\n",row[0]);
+	};
+
+
+	mysql_free_result(result);
+
+	//Close Connection
+	mysql_close(&inst);
+
+	return 0;
+}
